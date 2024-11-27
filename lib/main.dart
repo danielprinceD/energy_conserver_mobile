057df9603c47cad41712c5e0ad9c3c8accb52cde
danielprinced <d.danielprince2003@gmail.com>
2024-11-27 20:11:20 +0530

    feat:all widgets are overviewed

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,66 @@
 import 'package:flutter/material.dart';
 
 void main() => 
-  runApp(MaterialApp(
-    home:  Scaffold(
+  runApp(
+    MaterialApp(
+      home : MyTest()
+    )
+  );
+
+class MyTest extends StatelessWidget{
+  int counter = 20;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
-        title: Text(
-          "Hello",
+        title: const Text(
+          "Hello " ,
           style:  TextStyle(
             color: Colors.white
           ),
            ),
         backgroundColor: Colors.blue[600],
       ),
-      body: Center(
-          child:  Text("Item 1"),
-      ),
-      floatingActionButton: FloatingActionButton.small( child: Text("+") , onPressed: (){}),
-    ) , 
-  ));
+      body: const Column(
+        children: [
+
+            Test() ,
+           
+           Padding(padding : EdgeInsets.all(40) , child: Text( "-> Hello 1") ) ,
+           
+            Padding(padding : EdgeInsets.all(40) , child: Text( "-> Hello 1") ) ,
+          
+        ],
+      )
+      ,
+
+      floatingActionButton: FloatingActionButton.small( child: Text("+") , onPressed: (){}), 
+  );
+  }
+}
+
+class Test extends StatefulWidget {
+  const Test({super.key});
+  @override
+    _TestState createState() => _TestState();
+}
+
+class _TestState extends State<Test> {
+  int counter = 10;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          child:  Text("${counter}"),
+          
+        ),
+        ElevatedButton(onPressed: (){
+          setState(() {
+            counter++;
+          });
+        } , child: Text("Add"))
+      ],
+    );
+  }
+}
