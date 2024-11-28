c43b4627a8130117384cd9a8591b33ff79574fcc
danielprinced <d.danielprince2003@gmail.com>
2024-11-28 07:18:24 +0530

    feat:app structure created

---
+++ lib/app-util.dart
@@ -0,0 +1,16 @@
+class AppData{
+    
+    static String appTitle = "Energy Conserver";
+
+    static Map router = {
+      "home" : "/home" , 
+      "duty-list" : "/myduty" ,
+      "building-list" : "/building-duty" ,
+      "block-list" : "/block-duty",
+      "devices" : "/devices" ,
+      "device-report" : "/device-report"
+    };
+
+    static List<String> navBar = ['My Duty' , 'Home' , 'Profile'];
+    
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,65 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() => 
   runApp(
-    MaterialApp(
-      home : MyTest()
+    const  MaterialApp(
+        
     )
   );
-
-class MyTest extends StatelessWidget{
-  int counter = 20;
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Hello " ,
-          style:  TextStyle(
-            color: Colors.white
-          ),
-           ),
-        backgroundColor: Colors.blue[600],
-      ),
-      body: const Column(
-        children: [
-
-            Test() ,
-           
-           Padding(padding : EdgeInsets.all(40) , child: Text( "-> Hello 1") ) ,
-           
-            Padding(padding : EdgeInsets.all(40) , child: Text( "-> Hello 1") ) ,
-          
-        ],
-      )
-      ,
-
-      floatingActionButton: FloatingActionButton.small( child: Text("+") , onPressed: (){}), 
-  );
-  }
-}
-
-class Test extends StatefulWidget {
-  const Test({super.key});
-  @override
-    _TestState createState() => _TestState();
-}
-
-class _TestState extends State<Test> {
-  int counter = 10;
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Container(
-          child:  Text("${counter}"),
-          
-        ),
-        ElevatedButton(onPressed: (){
-          setState(() {
-            counter++;
-          });
-        } , child: Text("Add"))
-      ],
-    );
-  }
-}
